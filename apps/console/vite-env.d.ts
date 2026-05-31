a0ef57cfda2cc6f7192f49d7d89742323536370a
Sumanth Chinthagunta <xmlking@gmail.com>
2026-05-30 18:55:44 -0700

    build: deps updated

--- apps/console/vite-env.d.ts
+++ apps/console/vite-env.d.ts
@@ -28,8 +28,3 @@ declare module 'virtual:env/server' {
   export { env }
   export default env
 }
-
-declare const __APP_VERSION__: string;
-declare const __GIT_TAG__: string;
-declare const __GIT_SHA__: string;
-declare const __GIT_TIME__: string;
