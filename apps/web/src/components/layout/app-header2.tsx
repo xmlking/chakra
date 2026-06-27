1b2477f771c0240b601839dd8fe785a4cc857373
Sumanth Chinthagunta <xmlking@gmail.com>
2026-06-27 17:52:50 -0500

    fix(web): added border bottom for header

--- apps/web/src/components/layout/app-header2.tsx
+++ apps/web/src/components/layout/app-header2.tsx
@@ -12,7 +12,7 @@ import { RouterBreadcrumb } from "#components/router-breadcrumb";
 export function AppHeader() {
   return (
     // to make header sticky, added: `sticky top-0 z-50 bg-background` */}
-    <header className="sticky top-0 z-50 flex h-(--header-height) shrink-0 items-center gap-2 bg-background transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+    <header className="sticky top-0 z-50 flex h-(--header-height) shrink-0 items-center gap-2 border-b bg-background transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
       {/* Left */}
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
